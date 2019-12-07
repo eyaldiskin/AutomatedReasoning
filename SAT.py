05d0ecaf24f3cb9b09e592709d8d1c98d5d043e9
eyaldiskin <eyal.diskin@mail.huji.ac.il>
2019-12-07 12:57:14 +0200

    added tests and documantation

--- SAT.py
+++ SAT.py
@@ -4,4 +4,16 @@ algebra = boolean.BooleanAlgebra()
 
 
 def preproccess(formula: boolean.Expression):
+    """
+    gets a formula in cnf form
+    returns an equivelant formula without redundant clauses
+    """
+    pass
+
+
+def tseitlin(formula: boolean.Expression):
+    """
+    gets a boolean formula
+    return the tseitlin transformation of that formula
+    """
     pass
